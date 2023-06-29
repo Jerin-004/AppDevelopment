359e6afa3781a8d9abcebdfad8f58cc066d252a1
Chijith Jerin S <chijithjerin2004@gmail.com>
2023-06-29 20:57:11 +0530

    not opening

--- learning_app/android/app/src/main/kotlin/com/inflexus/learning_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.inflexus.learning_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ new_app/android/app/src/main/kotlin/com/inflexus/new_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.inflexus.new_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
